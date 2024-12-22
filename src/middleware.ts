f2b1f37846f748b167c07594a14f12ea632662a7
Shea Harrington <sheaharrington@outlook.com>
2024-12-22 17:27:57 -0500

    Remove unused redirect import from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { decrypt, sessionCookie } from "@/lib/sessions";
-import { redirect } from "next/navigation";
 
 export default async function middleware(request: NextRequest) {
   const protected_routes = ["/", "/dashboard"];
